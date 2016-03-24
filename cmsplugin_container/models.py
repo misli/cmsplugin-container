8ef349df6ab70ac878507e6c35d9edd70f466030
Jakub Dorňák <jdornak@redhat.com>
2016-03-24 12:28:36 +0100

    provide configuration from CONTAINER_TEMPLATES
    as instance.conf, instance.context is deprecated

--- cmsplugin_container/models.py
+++ cmsplugin_container/models.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import, division, generators, nested_scopes, print_function, unicode_literals, with_statement
 
+import warnings
+
 from cms.models import CMSPlugin
 from django.conf import settings
 from django.db import models
@@ -11,12 +13,11 @@ CONTAINER_TEMPLATES = getattr(settings, 'CONTAINER_TEMPLATES', {
     'container': {
         'label':    _('container'),
         'template': 'container',
-        'context':  {'class': 'container'},
+        'class':    'container',
     },
     'carousel': {
         'label':    _('carousel'),
         'template': 'carousel',
-        'context':  {},
     },
 })
 
@@ -25,7 +26,11 @@ CONTAINER_TEMPLATES = getattr(settings, 'CONTAINER_TEMPLATES', {
 def get_label(key):
     try:
         return CONTAINER_TEMPLATES[key]['label']
-    except TypeError:
+    except (TypeError, KeyError):
+        warnings.warn('CONTAINER_TEMPLATES should contain dictionaries'
+            ' with key "label". Support for old style settings '
+            'is deprecated and will be removed in version 1.2',
+            DeprecationWarning)
         return CONTAINER_TEMPLATES[key]
 
 
@@ -39,15 +44,23 @@ class ContainerPlugin(CMSPlugin):
     def get_template(self):
         try:
             return CONTAINER_TEMPLATES[self.template]['template']
-        except TypeError:
+        except (TypeError, KeyError):
             return self.template
 
     @property
     def context(self):
+        warnings.warn('ContainerPlugin.context attribute is deprecated '
+            'and it will be removed in version 1.2; please use '
+            'config attribute instead',
+            DeprecationWarning)
         try:
             return CONTAINER_TEMPLATES[self.template]['context'] or {}
-        except TypeError:
+        except (TypeError, KeyError):
             return {}
+
+    @property
+    def config(self):
+        return CONTAINER_TEMPLATES[self.template]
 
     def __str__(self):
         return '{}'.format(get_label(self.template))
